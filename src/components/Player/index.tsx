142928ddd410e8822dd757e89da3c0b5f5e37393
ejscf <edsonjscf@gmail.com>
2021-04-20 18:29:00 -0300

    :sparkles: Second day finished

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -8,7 +8,35 @@ export function Player() {
 				<strong>Tocando agora</strong>
 			</header>
 
-			
+			<div className={styles.emptyPlayer}>
+				<strong>Selecione um podcast para ouvir</strong>
+			</div>
+
+			<footer className={styles.empty}>
+				<div className={styles.progress}>
+					<span>00:00</span>
+					<div className={styles.emptySlider}></div>
+					<span>00:00</span>
+				</div>
+
+				<div className={styles.buttons}>
+					<button type="button">
+						<img src="/shuffle.svg" alt="Embaralhar"/>
+					</button>
+					<button type="button">
+						<img src="/play-previous.svg" alt="Tocar anterior"/>
+					</button>
+					<button type="button" className={styles.playButton}>
+						<img src="/play.svg" alt="Tocar"/>
+					</button>
+					<button type="button">
+						<img src="/play-next.svg" alt="Tocar próxima"/>
+					</button>
+					<button type="button">
+						<img src="/repeat.svg" alt="Repetir"/>
+					</button>
+				</div>
+			</footer>
 		</div>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+import { useEffect } from "react";
+
+export default function Home(props) {
+  console.log(props.episodes)
   return (
-    <h1>index</h1>
-  )
+  <h1>index</h1>
+  );
+}
+
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
