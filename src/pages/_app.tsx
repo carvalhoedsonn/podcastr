e1f000377ddc37c13b346b390c44b86beb937cfa
ejscf <edsonjscf@gmail.com>
2021-04-22 07:30:46 -0300

    :sparkles: Third day finished

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,20 @@
-import '../styles/global.scss'
+import "../styles/global.scss";
 
-import { Player } from "../components/Player/index"
-import { Header } from "../components/Header/index"
+import { Player } from "../components/Player/index";
+import { Header } from "../components/Header/index";
 
-import styles from '../styles/app.module.scss'
+import styles from "../styles/app.module.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-  	<div className={styles.wrapper}>
-  	<main>
-  	    <Header />
-  		<Component {...pageProps} />  		
-  	</main>
-  	<Player />
-  	</div>
-  	
-  )
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
